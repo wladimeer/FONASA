2818de7716b540e96b5bc76376546f535c24f14d
Sebastián <sbenavidescartes@gmail.com>
2021-12-14 10:27:55 -0300

    Environment Variable Added and Configured

---
+++ backend/src/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+class DevelopmentConfig():
+  DEBUG = False
+  MYSQL_USER = os.getenv('USER')
+  MYSQL_PASSWORD = os.getenv('PASSWORD')
+  MYSQL_HOST = os.getenv('HOST')
+  MYSQL_DB = os.getenv('DB')
+  
+Config = {
+  'development': DevelopmentConfig
+}
